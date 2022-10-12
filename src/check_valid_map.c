8e69fea8fa518cd6b06cba734296102971fdc636
hirokikumagai <hiro.corp1212@gmail.com>
2022-10-12 16:43:17 +0900

    check_valid_char関数をまとめる

--- src/check_valid_map.c
+++ src/check_valid_map.c
@@ -6,38 +6,40 @@
 /*   By: hkumagai <hkumagai@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 17:26:42 by hkumagai          #+#    #+#             */
-/*   Updated: 2022/10/11 18:13:50 by hkumagai         ###   ########.fr       */
+/*   Updated: 2022/10/12 16:40:55 by hkumagai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
-void	check_valid_char(char *line)
-{
-	while (*line != '\0')
-	{
-		if (*line != '0' && *line != '1' && *line != 'E' \
-			&& *line != 'C' && *line != 'P')
-			exit(1);
-		line++;
-	}
-	return ;
-}
-
-void	check_valid_map(char *map)
+void	check_valid_char(char *map)
 {
 	char	**lines;
+	char	*line;
 	char	**tmp;
 
 	lines = ft_split(map, '\n');
 	tmp = lines;
 	while (*lines != NULL)
 	{
-		check_valid_char(*lines);
+		line = *lines;
+		while (*line != '\0')
+		{
+			if (*line != '0' && *line != '1' && *line != 'E' \
+				&& *line != 'C' && *line != 'P')
+				exit(1);
+			line++;
+		}
 		free(*lines);
 		*lines = NULL;
 		lines++;
 	}
 	free(tmp);
+	return ;
+}
+
+void	check_valid_map(char *map)
+{
+	check_valid_char(map);
 	return ;
 }

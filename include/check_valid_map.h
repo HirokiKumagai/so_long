594fae239aebdc77d69a5776887ab0bcb0adf667
hirokikumagai <hiro.corp1212@gmail.com>
2022-10-25 14:05:52 +0900

    header漏れ

--- include/check_valid_map.h
+++ include/check_valid_map.h
@@ -6,7 +6,7 @@
 /*   By: hkumagai <hkumagai@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 17:25:49 by hkumagai          #+#    #+#             */
-/*   Updated: 2022/10/24 00:52:51 by hkumagai         ###   ########.fr       */
+/*   Updated: 2022/10/25 13:54:00 by hkumagai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,10 @@ bool	is_valid_sides(t_map *map, size_t count_column, \
 						size_t count_row, char *line);
 bool	is_map_closed(t_map *map);
 bool	is_valid_map_element(t_map *map);
+void	move_up(t_map *map, size_t x, size_t y);
+void	move_down(t_map *map, size_t x, size_t y);
+void	move_right(t_map *map, size_t x, size_t y);
+void	move_left(t_map *map, size_t x, size_t y);
+void	move(t_map *map, size_t x, size_t y);
 bool	is_valid_path(t_game_data *data);
 #endif
